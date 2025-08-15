fe95690de04f462b44d89f8bd233ee8f377010c5
Huijing Hei <hhei@redhat.com>
2025-08-15 18:46:17 +0800

    packagesystem: add `Package` struct
    Inspired by Colin's
    https://github.com/coreos/bootupd/pull/977#discussion_r2272470601
    
    Copy the comment:
    ```
    Create a struct Package that has impl Ord.
    Compare two slices `[Package]` - and if everything compares equal
    then the longer one wins.
    ```

--- src/packagesystem.rs
+++ src/packagesystem.rs
@@ -69,57 +69,87 @@ where
     rpm_parse_metadata(&rpmout.stdout)
 }
 
-fn parse_evr_map(input: &str) -> BTreeMap<String, Evr> {
-    input
-        .split(',')
-        .map(|pkg| {
-            // assume it is like "grub2-1:2.12-28.fc42,shim-15.8-3"
-            if !pkg.ends_with(std::env::consts::ARCH) {
-                let (name, version_release) = pkg.split_once('-').unwrap_or((pkg, ""));
-                let evr = Evr::parse(version_release);
-                return (name.to_string(), evr);
-            }
-
-            // assume it is like "grub2-efi-x64-1:2.12-28.fc42.x86_64,shim-x64-15.8-3.x86_64"
-            let nevra = rpm::Nevra::parse(pkg);
-            let _name = nevra.name();
-            // get name as "grub2" to match the usr/lib/efi path
-            let (name, _) = _name.split_once('-').unwrap_or((_name, ""));
-            (
-                name.to_string(),
-                Evr::new(
-                    nevra.epoch().to_string(),
-                    nevra.version().to_string(),
-                    nevra.release().to_string(),
-                ),
-            )
-        })
-        .collect()
+#[derive(Debug, Eq, PartialEq)]
+struct Package<'a> {
+    name: String,
+    version: Evr<'a>,
 }
 
-// Implement Compare package versions function:
-// Return `Greater` if evr_a > evr_b,
-// Return `Less` if evr_a < evr_b,
-// Return `Equal` if evr_a == evr_b.
-fn compare_package_versions_impl(a: &str, b: &str) -> Vec<(String, Ordering)> {
-    let map_a = parse_evr_map(a);
-    let map_b = parse_evr_map(b);
+impl<'a> Ord for Package<'a> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.name
+            .cmp(&other.name) // Compare names first
+            .then_with(|| self.version.cmp(&other.version)) // If names equal, compare versions
+    }
+}
 
-    // Compare package versions between `map_a` (current) and `map_b` (target).
-    // For each package in `map_b`:
-    // - If it exists in `map_a`, compare their versions and record the result.
-    // - If not found in `map_a`, assume it's a new package that should be upgraded.
-    let mut result = Vec::new();
-    for (name, evr_b) in map_b {
-        if let Some(evr_a) = map_a.get(&name) {
-            let cmp = evr_a.cmp(&evr_b);
-            result.push((name, cmp));
-        } else {
-            log::trace!("Found new package {name} in the target");
-            result.push((name, Ordering::Less));
+impl<'a> PartialOrd for Package<'a> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+fn parse_evr(pkg: &str) -> Package {
+    // assume it is "grub2-1:2.12-28.fc42" with no suffix .ARCH
+    if !pkg.ends_with(std::env::consts::ARCH) {
+        let (name, version_release) = pkg.split_once('-').unwrap_or((pkg, ""));
+        return Package {
+            name: name.to_string(),
+            version: Evr::parse(version_release),
+        };
+    }
+
+    // assume it is "grub2-efi-x64-1:2.12-28.fc42.x86_64"
+    let nevra = rpm::Nevra::parse(pkg);
+    let _name = nevra.name();
+    // get name as "grub2" to match the usr/lib/efi path
+    let (name, _) = _name.split_once('-').unwrap_or((_name, ""));
+    Package {
+        name: name.to_string(),
+        version: Evr::new(
+            nevra.epoch().to_string(),
+            nevra.version().to_string(),
+            nevra.release().to_string(),
+        ),
+    }
+}
+
+fn parse_evr_vec(input: &str) -> Vec<Package> {
+    let mut pkgs: Vec<Package> = input
+        .split(',')
+        .map(|pkg| parse_evr(pkg)) // parse_evr returns owned Package
+        .collect();
+    // Remove duplicates while preserving order
+    let mut seen = BTreeSet::new();
+    pkgs.retain(|pkg| seen.insert((pkg.name.clone(), pkg.version.clone())));
+    pkgs
+}
+
+fn compare_package_slices(a: &[Package], b: &[Package]) -> Ordering {
+    let mut has_greater = false;
+
+    // Assume it is in order
+    for (pkg_a, pkg_b) in a.iter().zip(b.iter()) {
+        match pkg_a.cmp(pkg_b) {
+            Ordering::Less => return Ordering::Less, // upgradable
+            Ordering::Greater => has_greater = true, // downgrade
+            Ordering::Equal => {}
         }
     }
-    result
+
+    // If all compared equal, longer slice wins
+    if a.len() < b.len() {
+        return Ordering::Less; // extra packages in b → upgrade
+    }
+    if a.len() > b.len() {
+        return Ordering::Greater; // extra packages in a → downgrade
+    }
+
+    if has_greater {
+        Ordering::Greater
+    } else {
+        Ordering::Equal
+    }
 }
 
 // Compare package versions:
@@ -131,30 +161,9 @@ pub(crate) fn compare_package_versions(a: &str, b: &str) -> Ordering {
     if a == b {
         return Ordering::Equal;
     }
-    let diffs = compare_package_versions_impl(a, b);
-
-    let mut has_less = false;
-    let mut has_greater = false;
-
-    for (_, ord) in &diffs {
-        match ord {
-            Ordering::Less => {
-                has_less = true;
-            }
-            Ordering::Greater => {
-                has_greater = true;
-            }
-            Ordering::Equal => {}
-        }
-    }
-
-    if has_less {
-        Ordering::Less
-    } else if has_greater {
-        Ordering::Greater
-    } else {
-        Ordering::Equal
-    }
+    let pkg_a = parse_evr_vec(a);
+    let pkg_b = parse_evr_vec(b);
+    compare_package_slices(&pkg_a, &pkg_b)
 }
 
 #[cfg(test)]
@@ -203,8 +212,9 @@ mod tests {
         let ord = compare_package_versions(current, target);
         assert_eq!(ord, Ordering::Less);
 
+        // The target missed some package
         let ord = compare_package_versions(target, current);
-        assert_eq!(ord, Ordering::Equal);
+        assert_eq!(ord, Ordering::Greater);
 
         // Not sure if this would happen
         // current_grub2 > target_grub2
@@ -241,6 +251,14 @@ mod tests {
         // Test only grub2
         let current = "grub2-1:2.12-28.fc42";
         let target = "grub2-1:2.12-29.fc42";
+        let ord = compare_package_versions(current, target);
+        assert_eq!(ord, Ordering::Less);
+
+        let ord = compare_package_versions(target, current);
+        assert_eq!(ord, Ordering::Greater);
+
+        let current = "grub2-efi-ia32-1:2.12-21.fc41.x86_64,grub2-efi-x64-1:2.12-21.fc41.x86_64,shim-ia32-15.8-3.x86_64,shim-x64-15.8-3.x86_64";
+        let target = "grub2-1:2.12-28.fc42,shim-15.8-3";
         let ord = compare_package_versions(current, target);
         assert_eq!(ord, Ordering::Less);
 
